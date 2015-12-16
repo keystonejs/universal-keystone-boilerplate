082b056916a70c7027dc6700d39532d914f4fe34
Eric Elliott <support@paralleldrive.com>
2015-12-15 21:25:31 -0700

    Close server connections.

--- source/test/functional/routes/index.js
+++ source/test/functional/routes/index.js
@@ -1,2 +1,11 @@
+import test from 'tape';
+import keystone from 'keystone.js';
+
 import './home';
 import './teams';
+
+test('close server connections', assert => {
+  keystone.httpServer.close();
+  keystone.mongoose.connection.close();
+  assert.end();
+});
